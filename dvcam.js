2add0d35222a1497b3d4bcd9f249d90dc49777ee
NinhDuongCN <nghiaandeveloper@gmail.com>
2023-02-17 23:32:51 +0700

    2023.02.17. Thử la bàn

--- dvcam.js
+++ dvcam.js
@@ -1,8 +1,52 @@
+//https://completejavascript.com/hai-cach-lay-vi-tri-nguoi-dung/
+//https://timoday.edu.vn/bai-43-dinh-vi-vi-tri-geolocation-trong-html5/
+function errorInfo(error){
+    switch(error.code){
+        case error.PERMISSION_DENIED:
+            return "Không được cấp quyền truy cập vị trí.";
+        case error.POSITION_UNAVAILABLE:
+            return "Không lấy được vị trí.";
+        case error.TIMEOUT:
+            return "Không lấy được vị trí do kết nối quá lâu.";
+        case error.UNKNOWN_ERROR:
+            return "Không lấy được vị trí do lỗi không xác định.";
+    }
+}
+
+function positionInfo(position){
+    document.getElementById('mat_toaDo').value = position.coords.latitude + ", " 
+                                               + position.coords.longitude + ", "
+                                               + position.coords.accuracy;
+    var huong = ["Bắc ", "Bắc Đông Bắc ", "Đông Bắc ", "Đông Đông Bắc ",
+                "Đông ", "Đông Đông Nam ", "Đông Nam ", "Nam Đông Nam ",
+                "Nam ", "Nam Tây Nam ", "Tây Nam ", "Tây Tây Nam ", 
+                "Tây ", "Tây Tây Bắc ", "Tây Bắc ", "Bắc Tây Bắc "];
+    document.getElementById('mat_huong').value = huong[Math.round(position.coords.heading / 22.5) % 16] + position.coords.heading + " độ";
+    
+    //switch(position.coords.heading){
+    //    case 0:   return "Bắc";
+    //    case 90:  return "Đông";
+    //    case 180: return "Nam";
+    //    case 270: return "Tây";
+    //    case 45:  return "Đông Bắc";
+    //    case 135: return "Đông Nam";
+    //    case 225: return "Tây Nam";
+    //    case 305: return "Tây Bắc";
+    //}
+    
+
+}
+
 function locate(){
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition((position)=> {
-            document.getElementById('mat_toaDo').value = position.coords.latitude + ", " + position.coords.longitude;
-        });
+        navigator.geolocation.getCurrentPosition(
+            (position)=> {document.getElementById('mat_toaDo').value = position.coords.latitude + ", " + position.coords.longitude; alert(position.coords.heading);},
+            (error) => {alert(errorInfo(error));},
+            { enableHighAccuracy: true, timeout: 5000, maximumAge: 0, }
+        );
+    }
+    else{
+        alert('Trình duyệt không hỗ trợ lấy tọa độ');
     }
 }
 
